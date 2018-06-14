9010d3ead5bdeca0398e36cef0efb3c68de7b1d8
Keith Duncan <keith_duncan@me.com>
2018-06-14 23:00:48 +1000

    Working tar print via IPC

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use tar::{Archive, Builder};
 extern crate libflate;
 use libflate::gzip;
 
-use std::{fs::File, io::Write};
+use std::{fs::File, str, io, io::{Read, Write, BufRead, BufReader}};
 
 extern crate sha1;
 use sha1::{Sha1, Digest};
@@ -42,45 +42,45 @@ fn main() {
           .required(true)
         )
       )
-      .subcommand(SubCommand::with_name("transport")
-        .setting(AppSettings::SubcommandRequired)
-        .subcommand(SubCommand::with_name("upload-index")
-          .about("Write an index on stdout, receive part requests on stdin and send those.")
-          .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .value_name("FILE")
-            .help("Path to the tarball file to transport to the remote peer")
-            .takes_value(true)
-            .required(true)
-          )
-          .arg(Arg::with_name("index")
-            .short("i")
-            .long("index")
-            .value_name("INDEX")
-            .help("Path to the index file to transport to the remote peer")
-            .takes_value(true)
-            .required(true)
-          )
+    )
+    .subcommand(SubCommand::with_name("transport")
+      .setting(AppSettings::SubcommandRequired)
+      .subcommand(SubCommand::with_name("upload-index")
+        .about("Write an index on stdout, receive part requests on stdin and send those.")
+        .arg(Arg::with_name("file")
+          .short("f")
+          .long("file")
+          .value_name("FILE")
+          .help("Path to the tarball file to transport to the remote peer")
+          .takes_value(true)
+          .required(true)
         )
-        .subcommand(SubCommand::with_name("receive-index")
-          .about("Receive an index on stdin, construct a tarball using a library of parts or request parts over stdout.")
-          .arg(Arg::with_name("destination")
-            .short("d")
-            .long("destination")
-            .value_name("DESTINATION")
-            .help("Path to the directory of tarball and index files to use for tarball construction, and ultimate tarball write")
-            .takes_value(true)
-            .required(true)
-          )
-          .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .value_name("FILE")
-            .help("File name to create in the destination directory")
-            .takes_value(true)
-            .required(true)
-          )
+        .arg(Arg::with_name("index")
+          .short("i")
+          .long("index")
+          .value_name("INDEX")
+          .help("Path to the index file to transport to the remote peer")
+          .takes_value(true)
+          .required(true)
+        )
+      )
+      .subcommand(SubCommand::with_name("receive-index")
+        .about("Receive an index on stdin, construct a tarball using a library of parts or request parts over stdout.")
+        .arg(Arg::with_name("destination")
+          .short("d")
+          .long("destination")
+          .value_name("DESTINATION")
+          .help("Path to the directory of tarball and index files to use for tarball construction, and ultimate tarball write")
+          .takes_value(true)
+          .required(true)
+        )
+        .arg(Arg::with_name("file")
+          .short("f")
+          .long("file")
+          .value_name("FILE")
+          .help("File name to create in the destination directory")
+          .takes_value(true)
+          .required(true)
         )
       )
     )
@@ -163,12 +163,49 @@ fn upload_index(matches: &clap::ArgMatches) -> MatchResult {
   let tar_path = matches.value_of("file").unwrap();
   let index_path = matches.value_of("index").unwrap();
 
-  Err(())
+  // Send the index
+
+  let mut index_file = File::open(index_path).unwrap();
+  let mut index = Vec::new();
+  index_file.read_to_end(&mut index).unwrap();
+
+  let mut stdout = io::stdout();
+  stdout.write_fmt(format_args!("{}\0", index.len())).unwrap();
+  stdout.write(&index).unwrap();
+
+  // Wait to read requested parts on stdin
+
+  Ok(())
 }
 
 fn receive_index(matches: &clap::ArgMatches) -> MatchResult {
   let destination_path = matches.value_of("destination").unwrap();
   let file = matches.value_of("file").unwrap();
 
-  Err(())
+  // Read the index
+
+  let mut stdin = BufReader::new(io::stdin());
+
+  let mut size_buffer = Vec::new();
+  stdin.read_until(b'\0', &mut size_buffer).expect("read length");
+  let ascii = &size_buffer[0..size_buffer.len()-1];
+
+  let index_length = str::from_utf8(ascii).unwrap().parse::<usize>().unwrap();
+  let mut index = vec![0u8; index_length];
+  stdin.read_exact(index.as_mut_slice()).expect("read index");
+
+  let decoder = gzip::Decoder::new(index.as_slice()).expect("gzip decoder");
+  let mut index_archive = Archive::new(decoder);
+
+  for file in index_archive.entries().expect("entries") {
+    // Make sure there wasn't an I/O error
+    let mut file = file.unwrap();
+
+    let mut file_hash = Vec::new();
+    file.read_to_end(&mut file_hash).unwrap();
+
+    println!("{:?} {:?} {:?} {:x?}", file.header().entry_type(), file.path(), file.header().size(), file_hash);
+  }
+
+  Ok(())
 }
