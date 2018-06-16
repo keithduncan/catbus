18f19248a426f83cf1a5eae641ded087700bd803
Keith Duncan <keith_duncan@me.com>
2018-06-16 16:44:28 +1000

    Replace expects in receive_index with ?

--- src/receive.rs
+++ src/receive.rs
@@ -34,65 +34,66 @@ pub fn receive_index(destination_path: &Path, destination_file: &str) -> io::Res
 
   // Read the index
   eprintln!("[receive-index] receiving index tarball");
-  let index = tarball_codec::read_tarball("[receive-index]", &mut stdin).expect("read index from upload-index");
+  let index = tarball_codec::read_tarball("[receive-index]", &mut stdin)?;
 
   // The index is always compressed
-  let decoder = gzip::Decoder::new(index.as_slice()).expect("gzip decoder");
+  let decoder = gzip::Decoder::new(index.as_slice())?;
   let mut index_archive = tar::Archive::new(decoder);
 
-  let output_file = File::create(output_path).expect("create output file");
+  let output_file = File::create(output_path)?;
   let mut output_builder = tar::Builder::new(output_file);
 
-  for file in index_archive.entries().expect("entries") {
+  for file in index_archive.entries()? {
     let mut file = file.expect("entry file");
 
     let mut new_header = file.header().clone();
 
     if new_header.entry_type() == tar::EntryType::Regular {
       let mut file_hash = Vec::new();
-      file.read_to_end(&mut file_hash).expect("read entry content");
+      file.read_to_end(&mut file_hash)?;
 
-      let entry_path = file.path().expect("entry path");
-      let entry_path = entry_path.to_str().expect("to str");
+      let entry_path = file.path()?;
+      // TODO handle error
+      let entry_path = entry_path.to_str().expect("entry path");
 
       // Tell sender we want it
       eprintln!("[receive-index] sending want {:?} {:?}", new_header.entry_type(), entry_path);
-      stdout.write_fmt(format_args!("{}\n", entry_path)).expect("write wanted entry");
+      stdout.write_fmt(format_args!("{}\n", entry_path))?;
     } else {
       let entry_path = file.path().expect("entry path").into_owned();
-      output_builder.append_data(&mut new_header, entry_path, file).expect("append entry to output");
+      output_builder.append_data(&mut new_header, entry_path, file)?;
     }
   }
 
   // Tell the sender EOF so they send the want parts
-  stdout.flush().expect("flush wanted entries");
+  stdout.flush()?;
   unsafe {
     libc::close(1);
   }
 
   // Read the tarball of wanted parts
   eprintln!("[receive-index] receiving wanted tarball");
-  let want = tarball_codec::read_tarball("[receive-index]", &mut stdin).expect("read wanted from upload-index");
+  let want = tarball_codec::read_tarball("[receive-index]", &mut stdin)?;
 
   // Append it to the archive we've built it
   let mut want_archive = tar::Archive::new(want.as_slice());
-  for file in want_archive.entries().expect("entries") {
-    let mut file = file.expect("wanted entry");
+  for file in want_archive.entries()? {
+    let mut file = file?;
 
-    eprintln!("[receive-index] receiving {:?}", file.path().expect("entry path"));
+    eprintln!("[receive-index] receiving {:?}", file.path()?);
 
     let mut new_header = file.header().clone();
-    let entry_path = file.path().expect("entry path").into_owned();
-    output_builder.append_data(&mut new_header, entry_path, file).expect("append wanted entry");
+    let entry_path = file.path()?.into_owned();
+    output_builder.append_data(&mut new_header, entry_path, file)?;
   }
 
   eprintln!("[receive-index] writing output tarball");
-  let mut output = output_builder.into_inner().expect("write output");
-  output.flush().expect("flush output");
+  let mut output = output_builder.into_inner()?;
+  output.flush()?;
 
-  let mut index_file = File::create(index_path).expect("create index file");
+  let mut index_file = File::create(index_path)?;
   eprintln!("[receive-index] writing index tarball");
-  index_file.write_all(&index).expect("write index to destination");
+  index_file.write_all(&index)?;
 
   Ok(())
 }
