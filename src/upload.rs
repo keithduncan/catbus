9ea5e1d68d18d73d48fc553e7567b1bef8e3d42f
Keith Duncan <keith_duncan@me.com>
2018-06-16 16:41:57 +1000

    Return errors from upload_index
    Replace all expect() instances with ?

--- src/upload.rs
+++ src/upload.rs
@@ -21,49 +21,53 @@ pub fn upload_index(tar_path: &str, index_path: &str) -> io::Result<()> {
 
   // Send the index first
   eprintln!("[upload-index] sending index tarball");
-  let mut index_file = File::open(index_path).expect("index file present");
-  tarball_codec::write_tarball("[upload-index]", &mut index_file, &mut stdout).expect("write index to receive-index");
+  let mut index_file = File::open(index_path)?;
+  tarball_codec::write_tarball("[upload-index]", &mut index_file, &mut stdout)?;
 
   let mut want_list = BTreeSet::new();
 
   // Wait to read requested parts on stdin
   eprintln!("[upload-index] reading want lines");
   let stdin = BufReader::new(io::stdin());
-  stdin.lines().for_each(|line| {
-    let line = line.expect("read line");
-    // For each wanted entry append it to the want list
-    eprintln!("[upload-index] WANTED {:?}", line);
-    want_list.insert(PathBuf::from(line));
-  });
+  stdin
+    .lines()
+    .for_each(|line| {
+      // TODO return error from upload_index
+      let line = line.expect("read line");
+      // For each wanted entry append it to the want list
+      eprintln!("[upload-index] WANTED {:?}", line);
+      want_list.insert(PathBuf::from(line));
+    });
 
   // Iterate the tar_path archive and accumulate the wanted entries
-  let tar_file = File::open(tar_path).expect("tar file present");
+  let tar_file = File::open(tar_path)?;
   let mut tar_archive = tar::Archive::new(tar_file);
 
   let want_output = Vec::new();
   let mut want_builder = tar::Builder::new(want_output);
 
   eprintln!("[upload-index] generating wanted tarball");
-  for file in tar_archive.entries().expect("entries") {
-    let mut file = file.expect("entry file");
+  for file in tar_archive.entries()? {
+    let mut file = file?;
 
     {
-      let file_path = file.path().expect("entry path");
+      let file_path = file.path()?;
+      // TODO optimise out allocation
       if !want_list.contains(&file_path.to_path_buf()) {
         continue;
       }
     }
 
     let mut new_header = file.header().clone();
-    let file_path = file.path().expect("entry path").into_owned();
+    let file_path = file.path()?.into_owned();
 
-    want_builder.append_data(&mut new_header, file_path, file).expect("append entry to wanted");
+    want_builder.append_data(&mut new_header, file_path, file)?;
   }
 
-  let want_output = &want_builder.into_inner().expect("finish wanted archive");
+  let want_output = &want_builder.into_inner()?;
 
   eprintln!("[upload-index] sending wanted tarball");
-  tarball_codec::write_tarball("[upload-index]", &mut want_output.as_slice(), &mut stdout).expect("write wanted to receive-index");
+  tarball_codec::write_tarball("[upload-index]", &mut want_output.as_slice(), &mut stdout)?;
   unsafe {
     libc::close(1);
   }
